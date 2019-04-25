ab5b97aa099865219e612c80876c978a67587d9c
shafi akhtar <shafiakhtar@shafis-MacBook-Air.local>
2019-04-25 08:36:35 +0530

    structure

--- src/container/App.js
+++ src/container/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import classes from "./App.module.css";
-import Person from "./Person/Person";
+import Persons from "../component/Persons/Persons";
 
 class App extends Component {
   state = {
@@ -60,17 +60,11 @@ class App extends Component {
     if (this.state.showperson) {
       persons = (
         <div>
-          {this.state.persons.map((person, index) => {
-            return (
-              <Person
-                click={() => this.deletePersonHandler(index)}
-                name={person.name}
-                age={person.age}
-                key={person.id}
-                changed={event => this.nameChangedHandler(event, person.id)}
-              />
-            );
-          })}
+          <Persons
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed={this.nameChangedHandler}
+          />
         </div>
       );
     }
